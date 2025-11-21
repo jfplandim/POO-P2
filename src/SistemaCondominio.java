71f62e793ef18de459590b816b80e50af8253da6
yuri <yuridouglas45@gmail.com>
2025-11-21 16:52:20 -0300

    SistemaCondominio.java

--- src/SistemaCondominio.java
+++ src/SistemaCondominio.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class SistemaCondominio {
 
-}
+    // Lista para armazenar os chamados (Seu "Banco de Dados" na memória)
+    private List<ChamadoManutencao> chamados;
