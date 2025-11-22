d53ce6397331c276f487e0639b3c888dade733d2
João <joaoguimalta2712@gmail.com>
2025-11-22 19:26:44 -0300

    Criação da class AreaReservavel.java

---
+++ src/AreaReservavel.java
@@ -0,0 +1,20 @@
+public enum AreaReservavel {
+    ACADEMIA("Academia"),
+    PISCINA("Piscina"),
+    SALAO_FESTAS("Salão de Festas");
+
+    private final String nome;
+
+    AreaReservavel(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
